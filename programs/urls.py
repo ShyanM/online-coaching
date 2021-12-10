dd606cd2f5b2d384791c83ea88bd65933b421f9e
ShyanM <shyanmuscat24@gmail.com>
2021-12-10 21:10:40 +0000

    added product details functionality

--- programs/urls.py
+++ programs/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.all_programs, name='programs' ),
+    path('<program_id>', views.program_detail, name='program_detail'),
 
 ]
--- programs/views.py
+++ programs/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Program
 
 # Create your views here.
@@ -13,3 +13,15 @@ def all_programs(request):
     }
 
     return render(request, 'programs/programs.html', context)
+
+
+def program_detail(request, program_id):
+    """ A view to show individual program details"""
+
+    program = get_object_or_404(Program, pk=program_id)
+    
+    context = {
+        'program': program
+    }
+
+    return render(request, 'programs/program_detail.html', context)
