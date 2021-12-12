108858036db7280863377c27d35f451791f14b60
ShyanM <shyanmuscat24@gmail.com>
2021-12-12 11:36:10 +0000

    updated context processor to show bag items and total

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,15 +1,27 @@
-
+from django.shortcuts import get_object_or_404
+from programs.models import Program
 
 def bag_contents(request):
 
     bag_items = []
     total = 0 
-    product_count = 0
+    program_count = 0
+    bag = request.session.get('bag', {})
+
+    for item_id, quantity in bag.items():
+        program = get_object_or_404(Program, pk=item_id)
+        total += quantity * program.price
+        program_count += quantity
+        bag_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'program': program,
+        })
     
     context = {
         'bag_items': bag_items,
         'total': total,
-        'product_count': product_count,
+        'program_count': program_count,
     }
 
     return context
