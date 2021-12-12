aee721b01fbca0035dade03483498158c4694861
ShyanM <shyanmuscat24@gmail.com>
2021-12-12 23:43:19 +0000

    added checkout views and templates

--- checkout/signals.py
+++ checkout/signals.py
@@ -3,14 +3,14 @@ from django.dispatch import receiver
 
 from .models import OrderLineItem
 
-@receiver(post_save, sender=OrderlineItem)
+@receiver(post_save, sender=OrderLineItem)
 def update_on_save(sender, instance, created, **kwargs):
 
     """ update order total on lineitem update/create """
 
     instance.order.update_total()
 
-@receiver(post_delete, sender=OrderlineItem)
+@receiver(post_delete, sender=OrderLineItem)
 def update_on_save(sender, instance, **kwargs):
 
     """ update order total on lineitem delete """
--- online_coaching/settings.py
+++ online_coaching/settings.py
@@ -45,6 +45,10 @@ INSTALLED_APPS = [
     'programs',
     'bag',
     'checkout',
+
+    # Other
+
+    'crispy_forms'
 ]
 
 MIDDLEWARE = [
@@ -58,6 +62,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'online_coaching.urls'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 TEMPLATES = [
     {
@@ -73,8 +79,13 @@ TEMPLATES = [
                 'django.template.context_processors.request', # required by allauth
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
                 'bag.contexts.bag_contents',
             ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
+            ]
         },
     },
 ]
