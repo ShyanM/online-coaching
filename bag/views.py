0c0650c4fdc9b96630b180996fe2864606a8b404
ShyanM <shyanmuscat24@gmail.com>
2021-12-12 21:07:20 +0000

    added toasts

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.contrib import messages
+
+from programs.models import Program
 
 # Create your views here.
 
@@ -11,7 +14,8 @@ def view_bag(request):
 
 def add_to_bag(request, item_id):
     """ add to bag"""
-
+    
+    program = Program.objects.get(pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     bag = request.session.get('bag', {})
@@ -20,13 +24,14 @@ def add_to_bag(request, item_id):
         bag[item_id] += quantity
     else:
         bag[item_id] = quantity
+        messages.error(request, f'Added {program.name} to your bag')
 
     request.session['bag'] = bag
     return redirect(redirect_url)
 
 
 def adjust_bag(request, item_id):
-    """ adjust the quantity of a specified product to the specified amount"""
+    """ adjust the quantity of a specified program to the specified amount"""
 
     quantity = int(request.POST.get('quantity'))
     bag = request.session.get('bag', {})
