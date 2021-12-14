2e8d4f278ff93a432efe06699de6e879d8b843aa
ShyanM <shyanmuscat24@gmail.com>
2021-12-14 00:29:39 +0000

    continuing deployment of site to heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn online-coaching.wsgi:application
--- online_coaching/settings.py
+++ online_coaching/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-78vcvchtlaoq1!etqhwciq%%bx@!_^yy#03psp+bf@a@thr!sx
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['shyanm-online-coaching.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -121,12 +121,18 @@ WSGI_APPLICATION = 'online_coaching.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-   }
-}
+
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+    }
 
 
 
